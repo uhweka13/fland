17e470742145adf9595336c878f7f8e8e7883da9
uhweeka ika <uhweka@gmail.com>
2020-06-07 10:45:25 +0100

    first commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <!-- Google Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.0/css/mdb.min.css" rel="stylesheet">
+</head>
+<body>
+    <!--Navbar -->
+    <nav class="mb-1 navbar navbar-expand-lg navbar-dark default-color">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
+        aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+            <a class="nav-link" href="#">Home
+                <span class="sr-only">(current)</span>
+            </a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">Dropdown
+            </a>
+            <div class="dropdown-menu dropdown-default" aria-labelledby="navbarDropdownMenuLink-333">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+            </li>
+        </ul>
+        <ul class="navbar-nav ml-auto nav-flex-icons">
+            <li class="nav-item">
+                <button class="btn btn-sm align-middle btn-outline-white" type="button">Try for Free</button>
+            </li>
+            <li class="nav-item">
+                <a href="/login"><button class="btn btn-sm align-middle btn-outline-white" type="button">Login</button></a>
+
+            </li>
+        </ul>
+        </div>
+    </nav>
+    <!--/.Navbar -->
+
+    <div class="container-fluid">
+        <div class="row" style="height: 500px;">
+            <div class="col-md-4" style="background-color: rgb(219, 199, 191)">
+                <div class="" style="width: 95%; height: 250px; margin: 0 auto; margin-top: 20px;">
+                    <i style="font-style: normal; font-size: 40px; font-weight: bold; background-color: rgb(11, 11, 78); color: #fff;">Fast. Focused. Distributed. Dynamic work done right.</i>
+                </div>
+                <div style="width: 80%; margin: 0 auto;">
+                    <i style="font-size: 13px;">Wherever people are working, they need to work dynamically. Smartsheet empowers everyone to plan, adapt, and get things done in a rapidly changing world.</i>
+                </div>
+                <div style="width: 80%; margin: 0 auto;">
+                    <button type="button" class="btn btn-default btn-sm">Watch video</button><br>
+                    <button type="button" class="btn btn-default btn-sm">Try for free</button>
+                </div>
+            </div>
+            <div class="col-md-8 bg-danger" style="background-image: url('images/back12.png'); background-repeat: no-repeat; background-size: 100% 100%;">
+                
+            </div>
+        </div>
+    </div>
+
+
+
+    
+
+    <!-- JQuery -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.0/js/mdb.min.js"></script>
+</body>
+</html>
